16d35a9fe0de34c5bb67f2efe5450b160f96b8a7
LongX <2080908852@qq.com>
2026-06-08 19:10:05 +0800

    feat(LED/Delay):add LED,Delay

---
+++ Hardware/LED.c
@@ -0,0 +1,19 @@
+#include "stm32f10x.h"                  // Device header
+
+void LED_Init(void){
+	GPIO_InitTypeDef GPIO_InitStructure;
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE);
+	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_13;
+	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_10MHz;
+	GPIO_Init(GPIOC,&GPIO_InitStructure);
+	GPIO_SetBits(GPIOC,GPIO_Pin_13);
+}
+
+void LED_ON(void){
+	GPIO_WriteBit(GPIOC,GPIO_Pin_13,(BitAction)0);
+}
+
+void LED_OFF(void){
+	GPIO_WriteBit(GPIOC,GPIO_Pin_13,(BitAction)1);
+}
