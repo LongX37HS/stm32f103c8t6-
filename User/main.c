dea1b6108ae0b49a5f1446631396efcbf728b4cf
LongX <2080908852@qq.com>
2026-06-09 20:57:34 +0800

    perf(MPU6050/IIC):优化读取MPU6050速度

--- User/main.c
+++ User/main.c
@@ -1,14 +1,17 @@
 #include "stm32f10x.h"                  // Device header
 #include "OLED.h"
 #include "MPU6050.h"
+#include "Timer.h"
 
 int16_t Ax,Az,Ay,Gx,Gz,Gy;
 uint8_t Ack=1;
+uint16_t TimerCount=0;
 int main(){
+	Timer_Init();
 	MPU6050_Init();
 	OLED_Init();
+	OLED_ShowNum(4,3,TimerCount,5);
 	while(1){
-		MPU6050_GetData(&Ax,&Az,&Ay,&Gx,&Gz,&Gy);
 		OLED_ShowSignedNum(1,1,Ax,5);
 		OLED_ShowSignedNum(2,1,Az,5);
 		OLED_ShowSignedNum(3,1,Ay,5);
@@ -22,9 +25,12 @@ int main(){
 }
 
 
-//void TIM1_UP_IRQHandler(void){
-//	if(TIM_GetFlagStatus(TIM1,TIM_FLAG_Update)==SET){
-//		
-//		TIM_ClearITPendingBit(TIM1,TIM_FLAG_Update);
-//	}
-//}
+void TIM1_UP_IRQHandler(void){
+	
+	if(TIM_GetFlagStatus(TIM1,TIM_FLAG_Update)==SET){
+		MPU6050_GetData(&Ax,&Az,&Ay,&Gx,&Gz,&Gy);
+		TimerCount=TIM_GetCounter(TIM1);
+		TIM_ClearITPendingBit(TIM1,TIM_FLAG_Update);
+		
+	}
+}
