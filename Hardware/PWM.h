f555cde30d6b90eeea77dad20df5e12a88fa4046
LongX <2080908852@qq.com>
2026-06-11 22:37:34 +0800

    feat(Encoder):编写encoder模块,修改Timer定时器中断配置tim3,优化usart字符串发送

---
+++ Hardware/PWM.h
@@ -0,0 +1,9 @@
+#ifndef __PWM_H
+#define __PWM_H
+
+void PWM_SetCompare4(uint16_t Compare);
+void PWM_SetCompare1(uint16_t Compare);
+
+void PWM_Init(void);
+
+#endif
--- User/main.c
+++ User/main.c
@@ -3,39 +3,99 @@
 #include "MPU6050.h"
 #include "Timer.h"
 #include "usart.h"
-
-
+#include "motor.h"
+#include "MPU6050.h"
+#include "Encoder.h"
+#include "Delay.h"
+//MPU6050测试
 int16_t Ax,Az,Ay,Gx,Gz,Gy;
 uint8_t Ack=1;
 uint16_t TimerCount=0;
+uint8_t receive;;
+int8_t speedL=0,speedR=0;
+uint8_t ID;
+int16_t count1;
+int16_t count2;
 int main(){
-	MyUSART_Init();
 	Timer_Init();
+	motor_Init();
 	MPU6050_Init();
-	OLED_Init();
-	OLED_ShowNum(4,3,TimerCount,5);
-
+	MyUSART_Init();
+	OLED_Init();	
+	MyUSART_Printf("U");
+	Encoder_Init();
 	while(1){
-		printf("%d ,%d ,%d\r\n", Az,  Ax,  Ay);
+		if (MyUSART_GetRxFlag()) {
+			receive = MyUSART_GetRxData();
+			if (receive == 0x41) {
+				speedR+=20;
+				if(speedR>100){
+					speedR=0;
+				}
+				motor_SetSpeed(1,speedR);
+			}else if(receive == 0x42){
+				speedR-=20;
+				if(speedR<-100){
+					speedR=0;
+				}
+				motor_SetSpeed(1,speedR);
+			}else if(receive == 0x43){
+				speedL+=20;
+				if(speedL>100){
+					speedL=0;
+				}
+				motor_SetSpeed(0,speedL);
+			}else if(receive == 0x44){
+				speedL-=20;
+				if(speedL<-100){
+					speedL=0;
+				}
+				motor_SetSpeed(0,speedL);
+			}
+			OLED_ShowSignedNum(4, 1, speedL,3);
+			OLED_ShowSignedNum(4, 5, speedR,3);
+			
+		}
 //		OLED_ShowSignedNum(1,1,Ax,5);
 //		OLED_ShowSignedNum(2,1,Az,5);
 //		OLED_ShowSignedNum(3,1,Ay,5);
-		OLED_ShowSignedNum(1,8,Gx,5);
-		OLED_ShowSignedNum(2,8,Gz,5);
-		OLED_ShowSignedNum(3,8,Gy,5);
+//		OLED_ShowSignedNum(1,8,Gx,5);
+//		OLED_ShowSignedNum(2,8,Gz,5);
+//		OLED_ShowSignedNum(3,8,Gy,5);
+        count1=Encoder_GetCounter1();
+		count2=Encoder_GetCounter2();
+		Delay_ms(100);
+		OLED_ShowSignedNum(1,1,count1,5);
+		OLED_ShowSignedNum(1,8,count2,5);
 	}
-
 
 }
 
 
-void TIM1_UP_IRQHandler(void){
+void TIM3_IRQHandler(void){
 	
-	if(TIM_GetFlagStatus(TIM1,TIM_FLAG_Update)==SET){
-		MPU6050_GetData(&Ax,&Az,&Ay,&Gx,&Gz,&Gy);
-		TimerCount=TIM_GetCounter(TIM1);
-		TIM_ClearITPendingBit(TIM1,TIM_FLAG_Update);
+	if(TIM_GetFlagStatus(TIM3,TIM_FLAG_Update)==SET){
+//		MPU6050_GetData(&Ax,&Az,&Ay,&Gx,&Gz,&Gy);
+		TimerCount=TIM_GetCounter(TIM3);
+		TIM_ClearITPendingBit(TIM3,TIM_FLAG_Update);
 		
 	}
 }
+//编码器测试
+//int16_t count1;
+//int16_t count2;
 
+//int main(){
+//	Encoder_Init();
+//	motor_Init();
+//	MPU6050_Init();
+//	OLED_Init();
+//	OLED_Clear();
+//	while(1){
+//		count1=Encoder_GetCounter1();
+//		count2=Encoder_GetCounter2();
+//		Delay_ms(100);
+//		OLED_ShowSignedNum(1,1,count1,5);
+//		OLED_ShowSignedNum(1,8,count2,5);
+//	}
+//}
